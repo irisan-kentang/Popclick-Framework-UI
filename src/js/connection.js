2f599124e1b849e1a4ecef5d1d20b9863d3a9b8e
cacadosman <wetmanz23@gmail.com>
2021-08-30 08:01:03 +0700

    initial commit

--- src/js/connection.js
+++ src/js/connection.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const socket = io(process.env.WS_URL)
+const socket = io.connect(process.env.WS_URL, {secure: true})
 
 let currentScore = localStorage.getItem('score')
 currentScore = (currentScore == null) ? 0 : currentScore
